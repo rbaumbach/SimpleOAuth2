4731957ba148e38af649413048c6f8aa718ef757
rbaumbach <rbaumbach.github@gmail.com>
2014-10-08 22:20:57 -0700

    Removing unnecessary comments

--- SimpleOAuth2/main.m
+++ SimpleOAuth2/main.m
@@ -1,13 +1,6 @@
-//
-//  main.m
-//  SimpleOAuth2
-//
-//  Created by Baumbud on 10/8/14.
-//  Copyright (c) 2014 RKB. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
